f93f1d129a23b653217ea7b1cbdd8d8e0aea7dc9
Turkey <jhih-ronglu@gapp.nthu.edu.tw>
2022-07-12 22:25:15 +0800

    add deepcopy(), and some minor modificaitons

--- LinkedListDeque.java
+++ LinkedListDeque.java
@@ -15,11 +15,12 @@ public class LinkedListDeque<T> {
         }
     }
 
-        private final LList sentinel;
+        private LList sentinel;
         private LList sentinellast;
         public LinkedListDeque(){
-            sentinel = new LList(null, null, sentinellast);
-            sentinellast = new LList(sentinel,null, null);
+            sentinel = new LList(null,null,null);
+            sentinel.next = new LList(sentinel, null, null);
+            sentinellast = sentinel.next;
             size = 0;
         }
 
@@ -35,10 +36,7 @@ public class LinkedListDeque<T> {
 
         public void addFirst(T y){
             sentinel.next = new LList(sentinel,y, sentinel.next);
-            if (sentinel.next.next != null) {
-                sentinel.next.next.previous = sentinel.next;
-            }
-            sentinellast.previous = sentinel.next;
+            sentinel.next.next.previous = sentinel.next;
             size += 1;
         }
 
@@ -67,7 +65,6 @@ public class LinkedListDeque<T> {
 
         public void printDeque(){
             LList p = sentinel.next;
-            LList pp = sentinellast;
             while (p.next != null) {
                     System.out.print(p.item + " ");
                     p = p.next;
@@ -92,6 +89,9 @@ public class LinkedListDeque<T> {
 
         /**iterative method */
         public T get(int index){
+            if (this.isEmpty()){
+                return null;
+            }
             LList pt = sentinel.next;
             int i = 0;
             while (i != index){
@@ -101,74 +101,86 @@ public class LinkedListDeque<T> {
             return pt.item;
         }
 
-/*
-         deepcopy of LinkedListDeque other
-        public LinkedListDeque(LinkedListDeque other){
-            sentinel = new LList(null,51, null);
-            sentinellast = new LList(sentinel, 101, null);
-            for (int j = 0; j < other.size; j++){
-                if (j==0){
-                /** same as public LinkedListDeque(int x) * /
-                sentinel = new LList(null,null, null);
-                sentinellast = new LList(sentinel, null, null);
-                sentinel.next = new LList(sentinel, other.get(j), sentinellast);
-                sentinellast.previous = sentinel.next;
-                size = 1;}
-                if (j > 0){
-                /** same as public addLast(int x) * /
-                sentinellast.previous = new LList(sentinellast.previous, other.get(j), sentinellast);
-                sentinellast.previous.previous.next = sentinellast.previous;
-                size += 1;}
 
+         /** deepcopy of LinkedListDeque other */
+        public LinkedListDeque(LinkedListDeque<T> other){
+            sentinel = new LList(null,null,null);
+            sentinel.next = new LList(sentinel, null, null);
+            sentinellast = sentinel.next;
+            size = 0;
+//            if (other.size==0){
+//                sentinel = new LList(null, null, null);
+//                sentinellast = new LList(sentinel, null, null);
+//                sentinel.next = sentinellast;
+//                size = 0;
+//            }
+//            if (other.size > 0){
+//                sentinel = new LList(null,null, null);
+//                sentinellast = new LList(sentinel, null, null);
+//                sentinel.next = new LList(sentinel, other.getfirst(), sentinellast);
+//                size = 1;
+            for (int j = 0; j < other.size(); j++) {
+                addLast(other.get(j));
+                size += 1;
             }
 
         }
-*/
+
 
     /** recursive method */
         public T getRecursive(int index){
             if (index == 0){
                 return sentinel.next.item;
             }
+//            LList p = sentinel.next;
 
-            return this.getRecursive(index - 1);
+//            return this.sentinel.next.item.(index-1);
+            return null;
         }
 
 
-//        public static void main(String[] args){
-//
-//            /** Create two lists, DL and DLL:
-//                DL is an empty list, while DLL is (43, 44, 45, 90, 91, 92). */
-//            LinkedListDeque<Integer> DL = new LinkedListDeque();
-//            LinkedListDeque<Integer> DLL = new LinkedListDeque(90);
-//            DLL.addFirst(45);
-//            DLL.addFirst(44);
-//            DLL.addFirst(43);
-//            DLL.addLast(91);
-//            DLL.addLast(92);
-//
-//            /** Test functionalities: printDeque, removeLast, removeFirst, get, getRecursive,
-//             * and deepcopy. */
+        public static void main(String[] args){
+
+            /** Create two lists, DL and DLL:
+                DL is (19, 20, 91, 92), while DLL is (44, 45, 90, 100, 101). */
+            LinkedListDeque<Integer> DL = new LinkedListDeque<Integer>();
+            LinkedListDeque<Integer> DLL = new LinkedListDeque<Integer>(90);
+            DLL.addFirst(45);
+            DLL.addFirst(44);
+            DLL.addLast(100);
+            DLL.addLast(101);
+
+            DL.addFirst(20);
+            DL.addFirst(19);
+            DL.addLast(91);
+            DL.addLast(92);
+
+            /** Test functionalities: printDeque, removeLast, removeFirst, get, getRecursive,
+             * and deepcopy. */
 //            System.out.println(DLL.getfirst());
 //            System.out.println(DLL.getLast());
 //            System.out.println(DLL.isEmpty());
-////            DLL.printDeque();
+//            DLL.printDeque();
 //            DLL.removeFirst();
-////            DLL.printDeque();
+//            DLL.printDeque();
 //            DLL.removeLast();
-////            DLL.printDeque();
+//            DLL.printDeque();
 //            DLL.addLast(99);
 //            DLL.addFirst(43);
 //            DLL.printDeque();
-////
+//
 //            for (int i = 0; i<DLL.size; i++){
 ////                System.out.println(DLL.get(i));
 //                System.out.println(i);
 //                System.out.println(DLL.getRecursive(i));
 //            }
-////            LinkedListDeque<Integer> DLLcopy = new LinkedListDeque(DLL);
-////            DLLcopy.printDeque();
-//        }
+            DL.printDeque();
+            DLL.printDeque();
+            LinkedListDeque<Integer> DLLcopy = new LinkedListDeque(DLL);
+            DLLcopy.printDeque();
+            LinkedListDeque<Integer> DLcopy = new LinkedListDeque(DL);
+            DLcopy.printDeque();
+        }
 }
 
 
